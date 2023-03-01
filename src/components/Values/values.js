607a8d4790caf22b8890fac0e9bcb9959ce0dfa9
edu <edu@gmail.com>
2023-03-01 11:42:11 +0300

    01/03/2023

--- src/components/Values/values.js
+++ src/components/Values/values.js
@@ -8,34 +8,34 @@ import { HiOutlineUserGroup } from 'react-icons/hi'
 
 const Values = () => {
   return (
-    <div className='tester-section'>
+    <div className='values-section'>
       <div className='service-heading'>
         <h5>How it works</h5>
         <h2>Visualize your Ideas</h2>
       </div>
-      <div className='service-card-wrapper'>
-        <div className='service-card'>
+      <div className='values-card-wrapper'>
+        <div className='values-card'>
           <div className='icon'>
             <FaUserTie />
           </div>
           <h4>Professionalism</h4>
           <p>We deliver the final work in a great and professional way.</p>
         </div>
-        <div className='service-card'>
+        <div className='values-card'>
           <div className='icon'>
             <IoGitNetworkOutline />
           </div>
           <h4>Smooth Workflow</h4>
           <p>Providing the easiest and smoothest workflow in your interior.</p>
         </div>
-        <div className='service-card'>
+        <div className='values-card'>
           <div className='icon'>
             <VscWorkspaceTrusted />
           </div>
           <h4>Trusted Platform</h4>
           <p>We have gained trust and confidence from our customers.</p>
         </div>
-        <div className='service-card'>
+        <div className='values-card'>
           <div className='icon'>
             <HiOutlineUserGroup />
           </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -45,8 +45,12 @@ const About = () => {
               Our experienced designers work closely with clients to create custom interiors that reflect their unique style and needs.
               Whether you need a stunning residential space or a functional commercial interior,
               Villaspace has the expertise to bring your project to life.
+            </p>
+            <p>
               We understand that investing in an interior design project can be a big decision, which is why we offer a collaborative approach that ensures every project is tailored to your vision and exceeds your expectations.
               Our commitment to quality craftsmanship, attention to detail, and personalized touch is evident in our diverse portfolio of projects.
+            </p>
+            <p>
               Don't just take our word for it - our satisfied clients speak for themselves, with glowing reviews and testimonials that attest to the value and impact of our design work.
               If you're ready to transform your space and enhance your life with exceptional interior design, get in touch with Villaspace today. Let us help you bring your vision to life and create a space that inspires and delights.
             </p>
